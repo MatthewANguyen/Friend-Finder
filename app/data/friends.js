0ba8d33ee6ae61e5b25209b1ea9d97fa6bc6aa02
matthewnguyen <matthewnguyen19@gmail.com>
2017-11-03 23:26:59 -0700

    the matching friend display does not work

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: "matt",
+		photo: "https://avatars3.githubusercontent.com/u/17091513?s=88&v=4",
+		scores: [
+			1,
+			3,
+			4,
+			2,
+			1,
+			5,
+			5,
+			3,
+			5,
+			2
+		]
+	}
+];
+
+module.exports = friends;
